911ce498c03bbdcca90563ab343e3339106df13e
hikboy <hikboy@outlook.com>
2021-04-03 04:39:17 -0400

    adjust makefile

--- example/01_hello_SDL/01_hello_SDL.cpp
+++ example/01_hello_SDL/01_hello_SDL.cpp
@@ -1,6 +1,6 @@
 /*This source code copyrighted by Lazy Foo' Productions (2004-2020)
 and may not be redistributed without written permission.*/
-
+#if 0
 //Using SDL and standard IO
 #include <SDL2/SDL.h>
 #include <stdio.h>
@@ -50,6 +50,38 @@ int main( int argc, char* args[] )
 	SDL_DestroyWindow( window );
 
 	//Quit SDL subsystems
+	SDL_Quit();
+
+	return 0;
+}
+#endif
+
+#include <SDL2/SDL.h>
+#include <stdio.h>
+
+int main(void)
+{
+	SDL_Window * window = NULL;
+	SDL_Surface * Surface = NULL;
+
+	if(SDL_Init(SDL_INIT_VIDEO) < 0){
+		printf("SDL can not init  SDL_Error: %s\n", SDL_GetError());
+	}else{
+		window = SDL_CreateWindow("My first SDL exam", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, 800, 600, SDL_WINDOW_SHOWN);
+		if(window == NULL){
+			printf("SDL can not create window SDL_GetError %s\n", SDL_GetError());
+		}else{
+			Surface = SDL_GetWindowSurface(window);
+
+			SDL_FillRect(Surface, NULL, SDL_MapRGB(Surface->format, 0x00, 0xFF, 0xFF));
+
+			SDL_UpdateWindowSurface(window);
+
+			SDL_Delay(3000);
+		}
+	}
+
+	SDL_DestroyWindow(window);
 	SDL_Quit();
 
 	return 0;
